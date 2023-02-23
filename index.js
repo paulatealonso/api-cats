416e68102c80100b7516b358147102021de002bb
User <paulate.alonso@gmail.com>
2023-02-23 22:13:50 +0100

    add-adopted

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
 const express = require("express");
 const app = express();
+const cors = required('cors');
 const PORT = 3000;
 
 const cats = require("./cats.json");
+const adopted = require("./adopted.json");
 
-app.get("/", (request, response) => {
+app.get("/cats", (request, response) => {
     response.send({response: true, code: 200, cats: cats});
 });
 
-app.get("/:id", (request, response) => {
+app.get("/cats:id", (request, response) => {
     const { id } = request.params;
     const results = cats.filter(cat => cat.id === Number(id));
     response.status(200).send({response: true, cats: results});
 });
+
+app.get("/adopted", (request, response) => {
+    response.send({response: true, code: 200, adopted: adopted});
+});
+
+app.get("/adopted:id", (request, response) => {
+    const { id } = request.params;
+    const results = adopted.filter(adopt => adopt.id === Number(id));
+    response.status(200).send({response: true, adopted: results});
+});
+
+app.use(cors());
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
