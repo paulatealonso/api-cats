f03fe29ce9251a008459560d6c2819b40cc33371
User <paulate.alonso@gmail.com>
2023-02-23 23:13:20 +0100

    h

--- index.js
+++ index.js
@@ -1,22 +1,28 @@
 const express = require("express");
 const app = express();
-const cors = required('cors');
 const PORT = 3000;
-
+const cors = required('cors');
 const cats = require("./cats.json");
 
+app.use(cors())
 app.get("/", (request, response) => {
     response.send({response: true, code: 200, cats: cats});
 });
 
 app.get("/:id", (request, response) => {
     const { id } = request.params;
-    const results = cats.filter(cat => cat.id === Number(id));
+    const results = cats.filter(user => user.id === Number(id));
     response.status(200).send({response: true, cats: results});
 });
-
-app.use(cors())
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}...`);
 });
+
+
+
+
+
+
+
+
